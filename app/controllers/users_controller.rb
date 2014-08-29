52d98355cd953614dd8da28465d14a1813fbadee
Jungdo Lee <lee.jungdo@gmail.com>
2014-08-29 17:46:30 +0900

    fix require_admin when user tries to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-before_action :require_signin, only: [:new, :create]
+before_action :require_signin, only: [:index]
 before_action :require_correct_user, only: [:edit, :update, :destroy]
 
 def index
